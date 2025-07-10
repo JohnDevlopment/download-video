ef4a186a0e93a46720c60468ba5af9fef10088aa
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-07-10 18:05:25 -0500

    [ADD] .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,26 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (unless
+		     (fboundp 'mc-foldview)
+		   (defun mc-foldview
+		       (&optional arg)
+		     "Keyboard macro."
+		     (interactive "p")
+		     (kmacro-exec-ring-item
+		      '("fmv" 0 "%d")
+		      arg))))))
+ (org-mode . ((mode . local-lambda)
+	      (eval . (progn
+			(local-lambda-define-skeleton name "docstring" "Caption: " "\\begin{figure}[htbp]" n "\\centering" n "\\begin{adjustbox}{max width=\\textwidth, max height="
+						      (progn
+							(setq v1
+							      (read-number "Max height [0,1]: " 0.7))
+							(prin1-to-string
+							 (min v1 1.0)))
+						      "\\textheight, keepaspectratio}" n "\\includegraphics{"
+						      (f-relative
+						       (read-file-name "Image: "))
+						      125 n "\\end{adjustbox}" n "\\caption{" str 125 n "\\end{figure}")))
+	      (mode . display-fill-column-indicator)
+	      (mode . auto-fill))))
