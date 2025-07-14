ac4b9275ae1220368d994a0ec1e4af88861d062a
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-07-14 12:46:41 -0500

    [ADD] Utils: dprint function

--- download_video/utils.py
+++ download_video/utils.py
@@ -3,7 +3,34 @@ from __future__ import annotations
 import logging
 import time
 from collections.abc import Callable
-from typing import cast
+from typing import Any, Protocol, cast, overload
+
+
+class DprintFunction(Protocol):
+    @overload
+    def __call__[T](self, args: T) -> T:
+        ...
+
+    @overload
+    def __call__[*Tp](self, *args: *Tp) -> tuple[*Tp]:
+        ...
+
+dprint: DprintFunction
+
+def _dprint(*args: Any):
+    match args:
+        case []:
+            return None
+        case [arg1]:
+            return arg1
+        case [_arg1, *_args]:
+            return args
+
+dprint = cast("DprintFunction", _dprint)
+
+if __debug__:
+    from icecream import ic
+    dprint = cast("DprintFunction", ic)
 
 def only_once[T, **P](func: Callable[P, T]) -> Callable[P, T]:
     _init = False
