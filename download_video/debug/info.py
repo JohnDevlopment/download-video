7a1be446a251cd3bd0b37f133cc7de5f903699c6
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-10-28 23:18:41 -0500

    [UPDATE] Debug: Add docstring to load_info_dict()

--- download_video/debug/info.py
+++ download_video/debug/info.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import json
 
 def load_info_dict(site_name: str):
+    """
+    Load an info dictionary for the site processor SITE_NAME.
+    """
     fp = (Path(__file__).parent / site_name).with_suffix(".json")
     with fp.open() as fd:
         return json.load(fd)
