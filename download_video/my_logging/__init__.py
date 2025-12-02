befa04c4aee098eb91a844e0ad9db2397b163cb7
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-12-02 04:44:45 -0600

    [UPDATE] Logging: Use YAML configuration

--- download_video/my_logging/__init__.py
+++ download_video/my_logging/__init__.py
@@ -1,9 +1,20 @@
 from __future__ import annotations
 
 import logging
+import logging.config
 from enum import StrEnum
+from io import StringIO
+from pathlib import Path
+from string import Template
+from typing import TYPE_CHECKING
+
+import yaml
+from platformdirs import user_log_path
 
 from ..utils import only_once
+
+if TYPE_CHECKING:
+    from typing import Any
 
 
 class LogLevel(StrEnum):
@@ -13,6 +24,23 @@ class LogLevel(StrEnum):
     ERROR = "ERROR"
     CRITICAL = "CRITICAL"
 
+
 @only_once
-def setup_logging(_appname: str, level: LogLevel):
-    logging.basicConfig(level=level)
+def setup_logging(appname: str, level: LogLevel):
+    logdir = user_log_path(appname)
+    logdir.mkdir(parents=True, exist_ok=True)
+
+    # Load template from logging config file
+    fp = Path(__file__).parent / "logging.yaml"
+    tmpl = Template(fp.read_text())
+    mapping = {
+        "LOG_DIR": str(logdir),
+        "APPNAME": appname,
+        "LEVEL": str(level),
+    }
+    yaml_string = tmpl.substitute(mapping)
+
+    # Load YAML config
+    with StringIO(yaml_string) as fd:
+        LOGGING_CONFIG: dict[str, Any] = yaml.safe_load(fd)
+        logging.config.dictConfig(LOGGING_CONFIG)
